3caf739893552784b79f59151638d7d7c8221e48
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-16 02:49:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
 $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
-  status
+  $status
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
